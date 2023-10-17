15bf0fe23481374ce9f087dce6abbe9659b897b2
Kevin Duggan <kevinduggan95@gmail.com>
2023-10-17 14:20:12 -0500

    Try and await

--- off-record/server2/server.js
+++ off-record/server2/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors'); 
-// const puppeteer = require('puppeteer');
 
 const app = express();
 const port = 3001; // Choose an available port
@@ -11,137 +10,111 @@ const port = 3001; // Choose an available port
 app.use(cors());
 
 const deatilsUrl = 'https://www.ufc.com/event/ufc-294';
-
-// const deatilsUrl = 'https://www.ufc.com/event/ufc-fight-night-september-16-2023';
 const Recurl = 'https://www.tapology.com/fightcenter/events/99416-ufc-294';
-
 
 const fightRecords = [];
 const addedFighters = [];
+
 app.get('/scrape-ufc-website', async (req, res) => {
-    try {
-      const url = deatilsUrl;
-      
-      const response = await axios.get(url);
-      const $ = cheerio.load(response.data);
-  
-      // Scrape the event name and date
-      let event_name = $('.c-hero__headline').text().trim();
-      let event_date = $('.e-eventDate').text().trim();
-  
-      
-      event_name = event_name.replace(/\s+/g, ' ').trim();
-      // Clean up event date
-      event_date = event_date.replace(/\s+/g, ' ').trim();
-  
-      
-      const fightData = [];
-      
-      
-     
+  try {
+    const url = deatilsUrl;
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
 
-$('.c-listing-fight__content').each((index, element) => {
-    const weightClass = $(element).find('.c-listing-fight__class-text').text().trim().replace(/\s+/g, ' ');
-    const redCornerName = $(element).find('.c-listing-fight__corner-name--red a').text().trim().replace(/\s+/g, ' ');
-    const blueCornerName = $(element).find('.c-listing-fight__corner-name--blue a').text().trim().replace(/\s+/g, ' ');
-    const redCornerCountry = $(element).find('.c-listing-fight__country--red .c-listing-fight__country-text').text().trim().replace(/\s+/g, ' ');
-    const blueCornerCountry = $(element).find('.c-listing-fight__country--blue .c-listing-fight__country-text').text().trim().replace(/\s+/g, ' ');
+    // Scrape the event name and date
+    let event_name = $('.c-hero__headline').text().trim();
+    let event_date = $('.e-eventDate').text().trim();
 
-    const redCornerImage = $(element).find('.c-listing-fight__corner-image--red img').attr('src');
-    const blueCornerImage = $(element).find('.c-listing-fight__corner-image--blue img').attr('src');
+    event_name = event_name.replace(/\s+/g, ' ').trim();
+    event_date = event_date.replace(/\s+/g, ' ').trim();
 
-    
-    const winnerElement = $(element).find('.c-listing-fight__corner-body--red .c-listing-fight__outcome--Win');
-    const methodElement = $(element).find('.c-listing-fight__result-text.method');
+    const fightData = [];
 
-    let winner = 'N/A'; 
-    if (winnerElement.length > 0) {
-        winner = '0'; 
-    } else {
-    const blueWinnerElement = $(element).find('.c-listing-fight__corner-body--blue .c-listing-fight__outcome--Win');
-    if (blueWinnerElement.length > 0) {
-        winner = '1'; 
-    }
-}
+    $('.c-listing-fight__content').each((index, element) => {
+      const weightClass = $(element).find('.c-listing-fight__class-text').text().trim().replace(/\s+/g, ' ');
+      const redCornerName = $(element).find('.c-listing-fight__corner-name--red a').text().trim().replace(/\s+/g, ' ');
+      const blueCornerName = $(element).find('.c-listing-fight__corner-name--blue a').text().trim().replace(/\s+/g, ' ');
+      const redCornerCountry = $(element).find('.c-listing-fight__country--red .c-listing-fight__country-text').text().trim().replace(/\s+/g, ' ');
+      const blueCornerCountry = $(element).find('.c-listing-fight__country--blue .c-listing-fight__country-text').text().trim().replace(/\s+/g, ' ');
 
-    let method = 'N/A'; 
-    if (methodElement.length > 0) {
-        method = methodElement.text().trim();
-    }
+      const redCornerImage = $(element).find('.c-listing-fight__corner-image--red img').attr('src');
+      const blueCornerImage = $(element).find('.c-listing-fight__corner-image--blue img').attr('src');
 
+      const winnerElement = $(element).find('.c-listing-fight__corner-body--red .c-listing-fight__outcome--Win');
+      const methodElement = $(element).find('.c-listing-fight__result-text.method');
 
-
-const fightInfo = {
-    weightClass,
-    redCornerName,
-    blueCornerName,
-    redCornerCountry,
-    blueCornerCountry,
-    redCornerImage,
-    blueCornerImage,
-    winner,
-    method,
-    
-};
-
-    
-    fightData.push(fightInfo);
-});
-
-
-
-
-
-axios.get(Recurl)
-  .then((response) => {
-    if (response.status === 200) {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      const fightRecords = [];
-
-      $('.fightCard').each((index, element) => {
-        const redCornerName = $(element).find('.fightCardFighterName').eq(0).text().trim();
-        const blueCornerName = $(element).find('.fightCardFighterName').eq(1).text().trim();
-        const redCornerRecord = $(element).find('.fightCardRecord').eq(0).text().trim();
-        const blueCornerRecord = $(element).find('.fightCardRecord').eq(1).text().trim();
-
-        if (redCornerName && blueCornerName && redCornerRecord && blueCornerRecord) {
-          const fighter = {
-            redCornerName,
-            redCornerRecord,
-            blueCornerName,
-            blueCornerRecord
-          };
-
-          fightRecords.push(fighter);
+      let winner = 'N/A'; 
+      if (winnerElement.length > 0) {
+          winner = '0'; 
+      } else {
+        const blueWinnerElement = $(element).find('.c-listing-fight__corner-body--blue .c-listing-fight__outcome--Win');
+        if (blueWinnerElement.length > 0) {
+          winner = '1'; 
         }
-      });
+      }
 
-      // Print the fighter records
-      console.log(fightRecords);
-    }
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+      let method = 'N/A'; 
+      if (methodElement.length > 0) {
+          method = methodElement.text().trim();
+      }
 
+      const fightInfo = {
+        weightClass,
+        redCornerName,
+        blueCornerName,
+        redCornerCountry,
+        blueCornerCountry,
+        redCornerImage,
+        blueCornerImage,
+        winner,
+        method,
+      };
 
-  
-      res.json({ event_name, event_date, fights: fightData, records: fightRecords });
+      fightData.push(fightInfo);
+    });
+
+    // Use async/await to wait for the Tapology request to complete
+    try {
+      const response = await axios.get(Recurl);
+
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        $('.fightCard').each((index, element) => {
+          const redCornerName = $(element).find('.fightCardFighterName').eq(0).text().trim();
+          const blueCornerName = $(element).find('.fightCardFighterName').eq(1).text().trim();
+          const redCornerRecord = $(element).find('.fightCardRecord').eq(0).text().trim();
+          const blueCornerRecord = $(element).find('.fightCardRecord').eq(1).text().trim();
+
+          console.log(redCornerRecord);
+          console.log(blueCornerRecord);
+
+          if (redCornerName && blueCornerName && redCornerRecord && blueCornerRecord) {
+            const fighter = {
+              redCornerName,
+              redCornerRecord,
+              blueCornerName,
+              blueCornerRecord,
+            };
+
+            fightRecords.push(fighter);
+          }
+        });
+      }
     } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ error: 'An error occurred while scraping data.' });
+      return;
     }
-  });
-  
-  
-  
-  
 
-  
-  
-  
+    // Send the response with all the data
+    res.json({ event_name, event_date, fights: fightData, records: fightRecords });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while scraping data.' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- off-record/src/Tommy.js
+++ off-record/src/Tommy.js
@@ -67,7 +67,7 @@ function Tommy({ user, ufcCard, stallUfcCard}) {
       }
     });
     return formattedFighters;
-  }); 
+  });   
   
   
 
