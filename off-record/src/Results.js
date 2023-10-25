cba8c23db5bb28b38e93ae06df29c83909a803ef
Kevin Duggan <kevinduggan95@gmail.com>
2023-10-25 13:23:44 -0500

    new button/bugs fixed, sort the filtered results next, afternoon push

--- off-record/src/Results.js
+++ off-record/src/Results.js
@@ -550,19 +550,305 @@ const deletePrediction = (pickId, predIndex) => {
     handleUpdatePredictions(pickId, updatedPredictions);
   }
 };
-const [showPredictions, setShowPredictions] = useState(true);
+const [showPredictions, setShowPredictions] = useState(false);
+console.log(filteredByMainEvent)
+
+
+
+const [ownPicksVisibility, setOwnPicksVisibility] = useState(
+  filteredByMainEvent.map((result) => [result.id, false])
+);
+
+useEffect(() => {
+  const visibility = filteredByMainEvent.reduce((visibility, result) => {
+    visibility[result.id] = false;
+    return visibility;
+  }, {});
+  setOwnPicksVisibility(visibility);
+}, [filteredByMainEvent.length > 2]);
+
+console.log(ownPicksVisibility)
 const togglePredictions = () => {
   setShowPredictions(!showPredictions);
+
+  // Determine the new visibility state based on showPredictions
+  const newVisibility = showPredictions ? false : true;
+
+  // Create an updated object with all entries set to newVisibility
+  const updatedOwnPicksVisibility = {};
+  for (const key in ownPicksVisibility) {
+    updatedOwnPicksVisibility[key] = newVisibility;
+  }
+  console.log(updatedOwnPicksVisibility)
+  setOwnPicksVisibility(updatedOwnPicksVisibility);
 };
-const [ownPicksVisibility, setOwnPicksVisibility] = useState({});
 
 const toggleOwnPicks = (id) => {
   setOwnPicksVisibility((prevState) => ({
     ...prevState,
     [id]: !prevState[id],
   }));
-  console.log(ownPicksVisibility)
+  console.log(ownPicksVisibility);
 };
+
+
+const countryData = {
+  "Andorra": "AD",
+  "United Arab Emirates": "AE",
+  "Afghanistan": "AF",
+  "Antigua and Barbuda": "AG",
+  "Anguilla": "AI",
+  "Albania": "AL",
+  "Armenia": "AM",
+  "Netherlands Antilles": "AN",
+  "Angola": "AO",
+  "Antarctica": "AQ",
+  "Argentina": "AR",
+  "American Samoa": "AS",
+  "Austria": "AT",
+  "Australia": "AU",
+  "Aruba": "AW",
+  "Åland Islands": "AX",
+  "Azerbaijan": "AZ",
+  "Bosnia and Herzegovina": "BA",
+  "Barbados": "BB",
+  "Bangladesh": "BD",
+  "Belgium": "BE",
+  "Burkina Faso": "BF",
+  "Bulgaria": "BG",
+  "Bahrain": "BH",
+  "Burundi": "BI",
+  "Benin": "BJ",
+  "Saint Barthélemy": "BL",
+  "Bermuda": "BM",
+  "Brunei Darussalam": "BN",
+  "Bolivia": "BO",
+  "Bonaire, Sint Eustatius and Saba": "BQ",
+  "Brazil": "BR",
+  "Bahamas": "BS",
+  "Bhutan": "BT",
+  "Bouvet Island": "BV",
+  "Botswana": "BW",
+  "Belarus": "BY",
+  "Belize": "BZ",
+  "Canada": "CA",
+  "Cocos (Keeling) Islands": "CC",
+  "Congo, The Democratic Republic Of The": "CD",
+  "Central African Republic": "CF",
+  "Congo": "CG",
+  "Switzerland": "CH",
+  "Côte D'Ivoire": "CI",
+  "Cook Islands": "CK",
+  "Chile": "CL",
+  "Cameroon": "CM",
+  "China": "CN",
+  "Colombia": "CO",
+  "Costa Rica": "CR",
+  "Cuba": "CU",
+  "Cape Verde": "CV",
+  "Curaçao": "CW",
+  "Christmas Island": "CX",
+  "Cyprus": "CY",
+  "Czech Republic": "CZ",
+  "Germany": "DE",
+  "Djibouti": "DJ",
+  "Denmark": "DK",
+  "Dominica": "DM",
+  "Dominican Republic": "DO",
+  "Algeria": "DZ",
+  "Ecuador": "EC",
+  "Estonia": "EE",
+  "Egypt": "EG",
+  "Western Sahara": "EH",
+  "Eritrea": "ER",
+  "Spain": "ES",
+  "Ethiopia": "ET",
+  "England":"GB",
+  "Finland": "FI",
+  "Fiji": "FJ",
+  "Falkland Islands (Malvinas)": "FK",
+  "Micronesia, Federated States Of": "FM",
+  "Faroe Islands": "FO",
+  "France": "FR",
+  "Gabon": "GA",
+  "United Kingdom": "GB",
+  "Grenada": "GD",
+  "Georgia": "GE",
+  "French Guiana": "GF",
+  "Guernsey": "GG",
+  "Ghana": "GH",
+  "Gibraltar": "GI",
+  "Greenland": "GL",
+  "Gambia": "GM",
+  "Guinea": "GN",
+  "Guadeloupe": "GP",
+  "Equatorial Guinea": "GQ",
+  "Greece": "GR",
+  "South Georgia and the South Sandwich Islands": "GS",
+  "Guatemala": "GT",
+  "Guam": "GU",
+  "Guinea-Bissau": "GW",
+  "Guyana": "GY",
+  "Hong Kong": "HK",
+  "Heard and McDonald Islands": "HM",
+  "Honduras": "HN",
+  "Croatia": "HR",
+  "Haiti": "HT",
+  "Hungary": "HU",
+  "Indonesia": "ID",
+  "Ireland": "IE",
+  "Israel": "IL",
+  "Isle of Man": "IM",
+  "India": "IN",
+  "British Indian Ocean Territory": "IO",
+  "Iraq": "IQ",
+  "Iran, Islamic Republic Of": "IR",
+  "Iceland": "IS",
+  "Italy": "IT",
+  "Jersey": "JE",
+  "Jamaica": "JM",
+  "Jordan": "JO",
+  "Japan": "JP",
+  "Kenya": "KE",
+  "Kyrgyzstan": "KG",
+  "Cambodia": "KH",
+  "Kiribati": "KI",
+  "Comoros": "KM",
+  "Saint Kitts And Nevis": "KN",
+  "Korea, Democratic People's Republic Of": "KP",
+  "South Korea": "KR",
+  "Kuwait": "KW",
+  "Cayman Islands": "KY",
+  "Kazakhstan": "KZ",
+  "Lao People's Democratic Republic": "LA",
+  "Lebanon": "LB",
+  "Saint Lucia": "LC",
+  "Liechtenstein": "LI",
+  "Sri Lanka": "LK",
+  "Liberia": "LR",
+  "Lesotho": "LS",
+  "Lithuania": "LT",
+  "Luxembourg": "LU",
+  "Latvia": "LV",
+  "Libya": "LY",
+  "Morocco": "MA",
+  "Monaco": "MC",
+  "Moldova": "MD",
+  "Montenegro": "ME",
+  "Saint Martin": "MF",
+  "Madagascar": "MG",
+  "Marshall Islands": "MH",
+  "Macedonia, the Former Yugoslav Republic Of": "MK",
+  "Mali": "ML",
+  "Myanmar": "MM",
+  "Mongolia": "MN",
+  "Macao": "MO",
+  "Northern Mariana Islands": "MP",
+  "Martinique": "MQ",
+  "Mauritania": "MR",
+  "Montserrat": "MS",
+  "Malta": "MT",
+  "Mauritius": "MU",
+  "Maldives": "MV",
+  "Malawi": "MW",
+  "Mexico": "MX",
+  "Malaysia": "MY",
+  "Mozambique": "MZ",
+  "Namibia": "NA",
+  "New Caledonia": "NC",
+  "Niger": "NE",
+  "Norfolk Island": "NF",
+  "Nigeria": "NG",
+  "Nicaragua": "NI",
+  "Netherlands": "NL",
+  "Norway": "NO",
+  "Nepal": "NP",
+  "Nauru": "NR",
+  "Niue": "NU",
+  "New Zealand": "NZ",
+  "Oman": "OM",
+  "Panama": "PA",
+  "Peru": "PE",
+  "French Polynesia": "PF",
+  "Papua New Guinea": "PG",
+  "Philippines": "PH",
+  "Pakistan": "PK",
+  "Poland": "PL",
+  "Saint Pierre And Miquelon": "PM",
+  "Pitcairn": "PN",
+  "Puerto Rico": "PR",
+  "Palestine, State of": "PS",
+  "Portugal": "PT",
+  "Palau": "PW",
+  "Paraguay": "PY",
+  "Qatar": "QA",
+  "Réunion": "RE",
+  "Romania": "RO",
+  "Serbia": "RS",
+  "Russia": "RU",
+  "Rwanda": "RW",
+  "Saudi Arabia": "SA",
+  "Solomon Islands": "SB",
+  "Seychelles": "SC",
+  "Sudan": "SD",
+  "Sweden": "SE",
+  "Singapore": "SG",
+  "Saint Helena": "SH",
+  "Slovenia": "SI",
+  "Svalbard And Jan Mayen": "SJ",
+  "Slovakia": "SK",
+  "Sierra Leone": "SL",
+  "San Marino": "SM",
+  "Senegal": "SN",
+  "Somalia": "SO",
+  "Suriname": "SR",
+  "South Sudan": "SS",
+  "Sao Tome and Principe": "ST",
+  "El Salvador": "SV",
+  "Sint Maarten": "SX",
+  "Syrian Arab Republic": "SY",
+  "Swaziland": "SZ",
+  "Turks and Caicos Islands": "TC",
+  "Chad": "TD",
+  "French Southern Territories": "TF",
+  "Togo": "TG",
+  "Thailand": "TH",
+  "Tajikistan": "TJ",
+  "Tokelau": "TK",
+  "Timor-Leste": "TL",
+  "Turkmenistan": "TM",
+  "Tunisia": "TN",
+  "Tonga": "TO",
+  "Turkey": "TR",
+  "Trinidad and Tobago": "TT",
+  "Tuvalu": "TV",
+  "Taiwan, Republic Of China": "TW",
+  "Tanzania, United Republic of": "TZ",
+  "Ukraine": "UA",
+  "Uganda": "UG",
+  "United States Minor Outlying Islands": "UM",
+  "United States": "US",
+  "Uruguay": "UY",
+  "Uzbekistan": "UZ",
+  "Holy See (Vatican City State)": "VA",
+  "Saint Vincent And The Grenadines": "VC",
+  "Venezuela, Bolivarian Republic of": "VE",
+  "Virgin Islands, British": "VG",
+  "Virgin Islands, U.S.": "VI",
+  "Vietnam": "VN",
+  "Vanuatu": "VU",
+  "Wallis and Futuna": "WF",
+  "Samoa": "WS",
+  "Yemen": "YE",
+  "Mayotte": "YT",
+  "South Africa": "ZA",
+  "Zambia": "ZM",
+  "Zimbabwe": "ZW"
+};
+
+function getCountryAbbreviation(countryName) {
+  return countryData[countryName] || "Not Found";
+}
 // useEffect(() => {
 //   const initialVisibility = {};
 //   filteredByMainEvent.forEach((result) => {
@@ -766,14 +1052,36 @@ const toggleOwnPicks = (id) => {
 
 
 
+<center style={{paddingBottom:'5%'}}>
+  {/* <button
+    className={showPredictions ? "allhide-button" : "allshow-button"}
+    onClick={togglePredictions}
+  >
+    {showPredictions ? "Hide All" : "Show All"}
+  </button> */}
+<label  class="switch">
+  <input onClick={togglePredictions} type="checkbox" class="cb"></input>
+  <span class="toggle">
+    <span class="left">Hide All</span>  
+    <span class="right">Show All</span>  
+  </span>
+</label>
 
 
-        <center><button
-                  className={showPredictions ? "hide-button":"show-button"  }
-                  onClick={togglePredictions}
-                >
-                  {showPredictions ?"Hide All": "Show All"  }
-                </button></center>
+
+
+
+
+
+
+</center>
+
+
+
+
+
+
+                
       <table className="wholeOne" >
         <thead>
           <tr>
@@ -798,10 +1106,11 @@ const toggleOwnPicks = (id) => {
           {ownPicksVisibility[result.owner] ? "Hide" : "Show"}
         </button>
         <center>
-          <strong><span className="small">{result.owner}'s</span> picks</strong>
-          <br />
           <strong>{result.main_event}</strong>
       <br></br>
+          <strong><span className="small">{result.owner}'s</span> picks</strong>
+          <br />
+          
           {deletePicks && user && (user.username === result.owner || user.username === 'AdminKev') ? (
     <button
       className="delete-button"
@@ -814,8 +1123,8 @@ const toggleOwnPicks = (id) => {
          
         </center>
       </div>
-      {showPredictions || ownPicksVisibility[result.id] ? (
-      <div className={`predictions-container ${showPredictions || ownPicksVisibility ? '' : 'hidden-predictions'}`}>
+      { ownPicksVisibility[result.id] ? (
+      <div className={`predictions-container ${ownPicksVisibility ? '' : 'hidden-predictions'}`}>
         {result.predictions.map((prediction, predIndex) => {
           // Check if adminKevPicks contains the main event as a key
           const adminKevPicksForEvent = adminKevPicks[result.main_event] || {};
@@ -873,22 +1182,32 @@ const toggleOwnPicks = (id) => {
 
       
     
-      <td className="RightOne">  
-        <div className="pickresultsdiv">
-          {result.main_event !== 'Jon Jones vs Fedor Emelianenko' ?
-          <center>
-            <strong>Ufc Results</strong>
-            <br />
-            <strong>{result.main_event}</strong>
-            <h2>{result.predictions.length * 2} Possible </h2>
-          </center> :  <center>
+      <td  className="RightOne">  
+        <div  className="pickresultsdiv">
+          {result.main_event !== 'Jon Jones vs Fedor Emelianenko' ?<>
+          <center >
+          
+          
+          <strong>Ufc Results</strong>
+          <br />
+          <strong>{result.main_event}</strong><br></br>
+          <strong >
+            {result.location}
+          </strong>
+        </center><h2 style={{
+            backgroundImage: `url("https://flagsapi.com/${getCountryAbbreviation(result.location)}/flat/64.png")`,
+            backgroundSize: '50% 150%',
+            backgroundRepeat:'no-repeat',
+            backgroundPosition: 'right',
+          }}>{result.predictions.length * 2} Possible</h2></>
+         :  <center>
             <strong>Dream Card Results</strong>
             <br />
             <strong>{result.main_event}</strong>
             <h2>Popularity Vote </h2>
           </center>}
         </div>
-        {showPredictions || ownPicksVisibility[result.id]  ? (
+        { ownPicksVisibility[result.id]  ? (
         <div className="">
         {getAdminKevPicksForEvent(result.main_event).map((adminKevMatch, matchIndex) => (
   <div className="real-results-container" key={matchIndex}>
