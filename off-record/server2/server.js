a73264c19db941a3c39edee83d2009c982823d0a
Kevin Duggan <kevinduggan95@gmail.com>
2024-01-08 12:19:38 -0600

    server changes1.08pt2 eq3

--- off-record/server2/server.js
+++ off-record/server2/server.js
@@ -161,9 +161,10 @@ app.get('/scrape-ufc-website', async (req, res) => {
           const redCornerName = $(element).find('.fightCardFighterName').eq(0).text().trim();
           const blueCornerName = $(element).find('.fightCardFighterName').eq(1).text().trim();
           const redCornerFlag = $(element).find('.fightCardFlag').attr('src');
-          const blueCornerFlag = $(element).find('.fightCardFlag').eq(1).attr('src'); 
+          
           const redCornerRecord = $(element).find('.fightCardRecord').eq(0).text().trim();
           const blueCornerRecord = $(element).find('.fightCardRecord').eq(1).text().trim();
+          const blueCornerFlag = $(element).find('.fightCardRecord.fightCardFlag').eq(1).attr('src'); 
 
           console.log(redCornerName);
           console.log(blueCornerName);
