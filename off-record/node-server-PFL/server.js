b518c95c326fd0f4e47848fb7b36de10f1da1d29
Kevin Duggan <kevinduggan95@gmail.com>
2024-02-21 18:02:59 -0600

    new sever+hanges

--- off-record/node-server-PFL/server.js
+++ off-record/node-server-PFL/server.js
@@ -1,50 +1,151 @@
-const puppeteer = require('puppeteer-core');
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors'); 
+// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core'); // Import Puppeteer
+// const { DOMParser } = require('dom-parser'); // Import DOMParser from dom-parser
 
-const espnPFL = 'https://www.espn.com/mma/fightcenter/_/league/pfl';
 
-async function scrapeESPNPFL() {
-  const browser = await puppeteer.launch({
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+const app = express();
+const port = 3001; // Choose an available port
 
-  const page = await browser.newPage();
-  await page.goto(espnPFL, { waitUntil: 'domcontentloaded' });
+// Enable CORS for all routes
+app.use(cors());
 
-  const espnPFLData = await page.evaluate(() => {
-    const fighters = [];
 
-    document.querySelectorAll('.MMACompetitor').forEach((element) => {
-      const recordElement = element.querySelector('.flex.items-center.n9.nowrap.clr-gray-04');
-      const record = recordElement ? recordElement.textContent.trim() : '';
 
-      const nameElement = element.querySelector('.truncate');
-      const name = nameElement ? nameElement.textContent.trim() : '';
+const espnurl = 'https://www.espn.com/mma/fightcenter/_/league/pfl'
 
-      const headshotImageSrc = element.querySelector('.MMACompetitor .headshot img[data-mptype="image"]').getAttribute('src');
-      const countryFlagImageSrc = element.querySelector('.MMACompetitor .MMACompetitor__flag[data-mptype="image"]').getAttribute('src');
-      const playerImageSrc = element.querySelector('img[data-mptype="image"]').getAttribute('src');
+const fightRecords = [];
+const addedFighters = [];
+const fighters = []
+const oddResults = []
+const liveR = []
 
-      const hasRedArrow = element.querySelector('.MMACompetitor__arrow--reverse') !== null;
-      const hasBlueArrow = element.querySelector('.MMACompetitor__arrow:not(.MMACompetitor__arrow--reverse)') !== null;
 
-      fighters.push({
-        name,
-        record,
-        hasRedArrow,
-        hasBlueArrow,
-        playerImageSrc,
-        headshotImageSrc,
-        countryFlagImageSrc,
-      });
-    });
-
-    return fighters;
-  });
-
-  await browser.close();
-  return espnPFLData;
+function updatePreviousFights(property, value, currentIndex) {
+  for (let i = currentIndex - 1; i >= 0; i--) {
+      if (fightData[i][property] === 'N/A') {
+          fightData[i][property] = value;
+      } else {
+          // If the value is already updated, break the loop
+          break;
+      }
+  }
 }
 
-scrapeESPNPFL().then(data => console.log(data)).catch(error => console.error(error));
+
+
+
+
+
+app.get('/scrape-ufc-website', async (req, res) => {
+  try {
+
+    try {
+      const response = await axios.get(espnurl);
+
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+    
+        $('.AccordionPanel.mb4').each((index, element) => {
+        // .Gamestrip__Overview
+
+        const liveOne = []
+        
+        $('.Gamestrip__Overview').each((index, element) => {
+          // Extract network and odds information
+          const network = $(element).find('.ScoreCell__NetworkItem').text();
+          const odds = $(element).find('.ScoreCell__Odds').text();
+      
+          // Extract time, method, and other details
+          const timeDetailsElement = $(element).find('.ScoreCell__Time .tc');
+          const timeDetails = timeDetailsElement.length > 0 ? timeDetailsElement.text() : '';
+        
+          const broadcaster = $(element).find('.SomeOtherElementClass').text();
+     
+          const oddDetails = {
+      
+            network,
+            odds,
+            timeDetails,
+            broadcaster
+
+          }
+          if (!oddResults.some(existingDetails => JSON.stringify(existingDetails) === JSON.stringify(oddDetails))) {
+            // fightRecords.push(oddDetails);
+            liveOne.push(oddDetails);
+          }
+          // oddResults.push(oddDetails)
+      });
+      
+        $('.MMACompetitor').each((index, element) => {
+          const recordElement = $(element).find('.flex.items-center.n9.nowrap.clr-gray-04');
+          const record = recordElement.text().trim();
+      
+        
+          const nameElement = $(element).find('.truncate');
+          const name = nameElement.text().trim();
+
+          const headshotImageSrc = $('.MMACompetitor .headshot img[data-mptype="image"]').attr('src');
+          const countryFlagImageSrc = $('.MMACompetitor .MMACompetitor__flag[data-mptype="image"]').attr('src');
+
+          const playerImageSrc = $(element).find('img[data-mptype="image"]').attr('src');
+      
+
+
+
+
+
+
+          // Check for RedArrow
+          const hasRedArrow = $(element).find('.MMACompetitor__arrow--reverse').length > 0;
+
+          // Check for BlueArrow
+          const hasBlueArrow = $(element).find('.MMACompetitor__arrow:not(.MMACompetitor__arrow--reverse)').length > 0;
+
+          const fighter = {
+              name,
+              record,
+              hasRedArrow,
+              hasBlueArrow,
+              playerImageSrc,
+              headshotImageSrc,countryFlagImageSrc
+          };
+          // if (!fighters.some(existingDetails => JSON.stringify(existingDetails) === JSON.stringify(fighter))) {
+          //   fighters.push(fighter);
+          // }
+
+          fighters.push(fighter);
+          liveOne.push(fighter);
+            });
+      
+
+            liveR.push(liveOne);
+          
+          
+          });
+
+
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred while scraping data.' });
+      return;
+    }
+    console.log(fighters)
+   
+    res.json({
+       fighters, liveR
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while scraping data.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
