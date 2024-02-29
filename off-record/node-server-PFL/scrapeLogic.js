82626bf92c12748551d529cf2adb3d71df5e1367
Kevin Duggan <kevinduggan95@gmail.com>
2024-02-29 16:22:12 -0600

    pupeteerDocker22

--- off-record/node-server-PFL/scrapeLogic.js
+++ off-record/node-server-PFL/scrapeLogic.js
@@ -9,7 +9,10 @@ const scrapeLogic = async (res) => {
       "--single-process",
       "--no-zygote",
     ],
-    executablePath: './off-record/node-server-PFL/Chromium/chrome', 
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
   });
   try {
     const page = await browser.newPage();
