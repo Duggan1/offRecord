073cfda72d8bfb64e74fa07c80bf5903a33a5698
Kevin Duggan <kevinduggan95@gmail.com>
2024-07-22 13:24:53 -0500

    champ bulbul

--- off-record/node-server/server.js
+++ off-record/node-server/server.js
@@ -14,9 +14,9 @@ const port = 3001; // Choose an available port
 app.use(cors());
 
 
-const deatilsUrl = 'https://www.ufc.com/event/ufc-fight-night-july-20-2024'
-const Recurl = 'https://www.tapology.com/fightcenter/events/112783-ufc-fight-night'
-const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600046020/league/ufc'
+const deatilsUrl = 'https://www.ufc.com/event/ufc-304'
+const Recurl = 'https://www.tapology.com/fightcenter/events/112936-ufc-304'
+const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600045073/league/ufc'
 // const espnPFL = 'https://www.espn.com/mma/fightcenter/_/league/pfl'
 
 
