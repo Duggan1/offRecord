098a70a6b112ddcb2487e3596b677403d8a6afa3
Kevin Duggan <kevinduggan95@gmail.com>
2024-08-26 00:35:29 -0500

    bb

--- off-record/node-server/server.js
+++ off-record/node-server/server.js
@@ -13,9 +13,9 @@ const port = 3001; // Choose an available port
 // Enable CORS for all routes
 app.use(cors());
 
-const deatilsUrl = 'https://www.ufc.com/event/ufc-fight-night-august-10-2024'
-const Recurl = 'https://www.tapology.com/fightcenter/events/112784-ufc-fight-night'
-const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600048251/league/ufc'
+const deatilsUrl = 'https://www.ufc.com/event/ufc-fight-night-july-20-2024'
+const Recurl = 'https://www.tapology.com/fightcenter/events/112783-ufc-fight-night'
+const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600046020/league/ufc'
 // const espnPFL = 'https://www.espn.com/mma/fightcenter/_/league/pfl'
 
 
