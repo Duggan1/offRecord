cecdbaedd1467a18bdfc9e18d6808855f9855990
Kevin Duggan <kevinduggan95@gmail.com>
2025-01-13 16:21:05 -0600

    311

--- off-record/node-server/server.js
+++ off-record/node-server/server.js
@@ -13,9 +13,9 @@ const port = 3001; // Choose an available port
 // Enable CORS for all routes
 app.use(cors());
 
-const deatilsUrl = 'https://www.ufc.com/event/ufc-fight-night-january-11-2025'
-const Recurl = 'https://www.tapology.com/fightcenter/events/118450-ufc-fight-night'
-const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600051441/league/ufc'
+const deatilsUrl = 'https://www.ufc.com/event/ufc-311'
+const Recurl = 'https://www.tapology.com/fightcenter/events/118600-ufc-311'
+const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600051442/league/ufc'
 // const espnPFL = 'https://www.espn.com/mma/fightcenter/_/league/pfl'
 
 
