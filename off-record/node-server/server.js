7749352391d63ef471d51f5974a3b51f8fd227ef
Kevin Duggan <kevinduggan95@gmail.com>
2025-05-14 18:20:47 -0500

    burnsVmorales

--- off-record/node-server/server.js
+++ off-record/node-server/server.js
@@ -13,9 +13,9 @@ const port = 3001; // Choose an available port
 // Enable CORS for all routes
 app.use(cors());
 
-const deatilsUrl = 'https://www.ufc.com/event/ufc-315'
-const Recurl = 'https://www.tapology.com/fightcenter/events/123873-ufc-315'
-const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600053073/league/ufc'
+const deatilsUrl = 'https://www.ufc.com/event/ufc-fight-night-may-17-2025'
+const Recurl = 'https://www.tapology.com/fightcenter/events/124622-ufc-fight-night'
+const espnurl = 'https://www.espn.com/mma/fightcenter/_/id/600053676/league/ufc'
 // const espnPFL = 'https://www.espn.com/mma/fightcenter/_/league/pfl'
 
 
